792f225d1a82d54b09bc8e4e55cf2ec64f140fde
krisjczhang <krisjczhang@tencent.com>
2023-03-23 10:07:14 +0800

    fix

--- lib/itangoplugin_method_channel.dart
+++ lib/itangoplugin_method_channel.dart
@@ -42,7 +42,7 @@ class MethodChannelItangoplugin extends ItangopluginPlatform {
 
   @override
   Future<String?> doMtr(String params) async {
-    // v23
+    // v24
     final result = await methodChannel.invokeMethod<String>('doMtr', params);
     return result;
   }
